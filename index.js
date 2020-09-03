ce4f3039190880f61ae26b06910d87fd7b7d4feb
Mariel <coutomariel@gmail.com>
2020-09-02 21:48:12 -0300

    First commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute')
+
 const app = express();
 const port = 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
+
+userRoute(app);
 
 app.listen(port, () => {
     console.log('App rodando na port 3000')
